6087188fca7912bab405b835af99d3c07e741bd7
ukyouz <itainan.101@gmail.com>
2016-03-18 22:48:51 +0800

    Create messages.js

---
+++ _locales/zh_TW/messages.js
@@ -0,0 +1,26 @@
+{
+	"chrome_context_menu": {
+		"message": "取得文章"
+	},
+	"meta_extension_description": {
+		"message": "右鍵取得文章，極速抓取資料庫中大量樣本專用！免去寫論文取樣 Copy & Paste 煩惱。"
+	},
+	"meta_extension_name": {
+    	"message": "ArticleMixer"
+	},
+	"browse_expand_all": {
+		"message": "展開文章"
+	},
+	"browse_fold_all": {
+		"message": "收合文章"
+	},
+	"notifications_save_success": {
+		"message": "文章已儲存"
+	},
+	"notifications_save_error": {
+		"message": "資料庫操作失敗"
+	},
+	"notifications_icon_url": {
+		"message": "/icon128.png"
+	}
+}
